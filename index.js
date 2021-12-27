6aad167512aa57d001520634605aebe62ae41a7c
jucemalco <jucemalco@gmail.com>
2021-12-27 23:21:08 +0000

    modificación JS, undefined

---
+++ index.js
@@ -0,0 +1,47 @@
+const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+function RandomCardGenerator() {
+    return Math.floor(Math.random() * suits.length);
+  }
+const randomSuit = suits[RandomCardGenerator()];
+
+const topSuit = document.querySelector(".arriba");
+if (randomSuit == "&hearts;") {
+topSuit.style.color = "red";
+}
+if (randomSuit == "&diams;") {
+topSuit.style.color = "red";
+}
+topSuit.innerHTML = randomSuit;
+  
+const bottomSuit = document.querySelector(".abajo");
+if (randomSuit == "&hearts;") {
+bottomSuit.style.color = "red";
+}
+if (randomSuit == "&diams;") {
+bottomSuit.style.color = "red";
+}
+bottomSuit.innerHTML = randomSuit;
+  
+function RandomCardGenerator() {
+return Math.floor(Math.random() * numbers.length);
+}
+  
+const randomNumber = numbers[RandomCardGenerator()];
+  
+const NewNumber = document.querySelector(".numero");
+NewNumber.innerHTML = randomNumber;
